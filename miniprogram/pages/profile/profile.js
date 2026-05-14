e14906b0c33101ab7334dc3c085d7c9727775151
CookOrderLink Dev <dev@cookorderlink.local>
2026-05-14 23:16:28 +0800

    feat: profile page with family create/join flow and seed recipes

--- miniprogram/pages/profile/profile.js
+++ miniprogram/pages/profile/profile.js
@@ -1,4 +1,88 @@
+const { getCurrentUser, getCollection, COLLECTIONS } = require('../../utils/db');
+
 Page({
-  data: {},
-  onLoad() {},
+  data: {
+    userInfo: null,
+    familyId: '',
+    inviteCode: '',
+    hasFamily: false,
+    showJoinModal: false,
+    joinCodeInput: '',
+  },
+
+  async onLoad() {
+    const userInfo = await getCurrentUser();
+    this.setData({ userInfo, hasFamily: !!userInfo.familyId, familyId: userInfo.familyId });
+
+    if (userInfo.familyId) {
+      const familyRes = await getCollection(COLLECTIONS.FAMILIES).doc(userInfo.familyId).get();
+      this.setData({ inviteCode: familyRes.data.inviteCode });
+    }
+  },
+
+  async onCreateFamily() {
+    const res = await wx.cloud.callFunction({ name: 'createFamily' });
+    if (res.result.error) {
+      wx.showToast({ title: res.result.error, icon: 'none' });
+      return;
+    }
+    this.setData({
+      familyId: res.result.familyId,
+      inviteCode: res.result.inviteCode,
+      hasFamily: true,
+    });
+    getApp().globalData.familyId = res.result.familyId;
+    wx.showToast({ title: '创建成功', icon: 'success' });
+  },
+
+  onShowJoinModal() {
+    this.setData({ showJoinModal: true });
+  },
+
+  onJoinCodeInput(e) {
+    this.setData({ joinCodeInput: e.detail.value });
+  },
+
+  async onJoinFamily() {
+    const code = this.data.joinCodeInput.trim();
+    if (!code) {
+      wx.showToast({ title: '请输入邀请码', icon: 'none' });
+      return;
+    }
+    const res = await wx.cloud.callFunction({ name: 'joinFamily', data: { inviteCode: code } });
+    if (res.result.error) {
+      wx.showToast({ title: res.result.error, icon: 'none' });
+      return;
+    }
+    this.setData({
+      familyId: res.result.familyId,
+      hasFamily: true,
+      showJoinModal: false,
+    });
+    getApp().globalData.familyId = res.result.familyId;
+    wx.showToast({ title: '加入成功', icon: 'success' });
+  },
+
+  onCloseModal() {
+    this.setData({ showJoinModal: false });
+  },
+
+  onCopyInviteCode() {
+    wx.setClipboardData({
+      data: this.data.inviteCode,
+      success: () => wx.showToast({ title: '已复制', icon: 'success' }),
+    });
+  },
+
+  async onSeedRecipes() {
+    if (!this.data.hasFamily) {
+      wx.showToast({ title: '请先创建家庭组', icon: 'none' });
+      return;
+    }
+    wx.showLoading({ title: '添加菜谱...' });
+    const { seedRecipes } = require('../../utils/seed-recipes');
+    await seedRecipes(this.data.familyId, this.data.userInfo._id);
+    wx.hideLoading();
+    wx.showToast({ title: '已添加菜谱', icon: 'success' });
+  },
 });
