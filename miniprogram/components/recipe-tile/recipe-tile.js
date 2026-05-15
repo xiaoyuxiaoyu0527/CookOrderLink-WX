a52df9e1e537251675434cf31c9a4314952a788e
CookOrderLink Dev <dev@cookorderlink.local>
2026-05-15 16:58:36 +0800

    feat: add quantity selector, note input, and cart sheet to order page

--- miniprogram/components/recipe-tile/recipe-tile.js
+++ miniprogram/components/recipe-tile/recipe-tile.js
@@ -2,10 +2,25 @@ Component({
   properties: {
     recipe: { type: Object, value: {} },
     selected: { type: Boolean, value: false },
+    quantity: { type: Number, value: 1 },
+    note: { type: String, value: '' },
   },
   methods: {
     onTap() {
       this.triggerEvent('toggle', { recipeId: this.data.recipe._id });
+    },
+    onIncrease() {
+      this.triggerEvent('quantitychange', {
+        recipeId: this.data.recipe._id,
+        quantity: this.data.quantity + 1,
+      });
+    },
+    onDecrease() {
+      if (this.data.quantity <= 1) return;
+      this.triggerEvent('quantitychange', {
+        recipeId: this.data.recipe._id,
+        quantity: this.data.quantity - 1,
+      });
     },
     onNoteInput(e) {
       this.triggerEvent('notechange', {
--- miniprogram/pages/order/order.js
+++ miniprogram/pages/order/order.js
@@ -7,6 +7,8 @@ Page({
     selectedRecipes: {},
     totalCount: 0,
     totalMinutes: 0,
+    showCart: false,
+    cartItems: [],
   },
 
   async onLoad() {
@@ -45,18 +47,20 @@ Page({
       delete selectedRecipes[recipeId];
     } else {
       selectedIds.push(recipeId);
-      selectedRecipes[recipeId] = { note: '' };
+      selectedRecipes[recipeId] = { quantity: 1, note: '' };
     }
 
-    const selected = this.data.recipes.filter(r => selectedIds.includes(r._id));
-    const totalMinutes = selected.reduce((sum, r) => sum + (r.estimatedMinutes || 0), 0);
+    this.updateTotals(selectedIds, selectedRecipes);
+  },
 
-    this.setData({
-      selectedIds,
-      selectedRecipes,
-      totalCount: selectedIds.length,
-      totalMinutes,
-    });
+  onQuantityChange(e) {
+    const { recipeId, quantity } = e.detail;
+    const selectedRecipes = { ...this.data.selectedRecipes };
+    if (selectedRecipes[recipeId]) {
+      selectedRecipes[recipeId] = { ...selectedRecipes[recipeId], quantity };
+    }
+    this.setData({ selectedRecipes });
+    this.updateTotals(this.data.selectedIds, selectedRecipes);
   },
 
   onNoteChange(e) {
@@ -68,7 +72,75 @@ Page({
     this.setData({ selectedRecipes });
   },
 
+  updateTotals(selectedIds, selectedRecipes) {
+    const selected = this.data.recipes.filter(r => selectedIds.includes(r._id));
+    const totalMinutes = selected.reduce((sum, r) => sum + (r.estimatedMinutes || 0), 0);
+    const totalCount = selectedIds.reduce((sum, id) => sum + (selectedRecipes[id]?.quantity || 1), 0);
+
+    this.setData({
+      selectedIds,
+      selectedRecipes,
+      totalCount,
+      totalMinutes,
+    });
+  },
+
+  // Cart sheet
+  onShowCart() {
+    const cartItems = this.data.selectedIds.map(id => {
+      const recipe = this.data.recipes.find(r => r._id === id);
+      const info = this.data.selectedRecipes[id] || { quantity: 1, note: '' };
+      return {
+        recipeId: id,
+        name: recipe.name,
+        quantity: info.quantity,
+        note: info.note,
+      };
+    });
+    this.setData({ showCart: true, cartItems });
+  },
+
+  onHideCart() {
+    this.setData({ showCart: false });
+  },
+
+  onClearAll() {
+    this.setData({
+      selectedIds: [],
+      selectedRecipes: {},
+      totalCount: 0,
+      totalMinutes: 0,
+      showCart: false,
+      cartItems: [],
+    });
+  },
+
+  onRemoveItem(e) {
+    const id = e.currentTarget.dataset.id;
+    const selectedIds = this.data.selectedIds.filter(i => i !== id);
+    const selectedRecipes = { ...this.data.selectedRecipes };
+    delete selectedRecipes[id];
+
+    this.updateTotals(selectedIds, selectedRecipes);
+
+    const cartItems = this.data.cartItems.filter(i => i.recipeId !== id);
+    this.setData({ cartItems });
+
+    if (cartItems.length === 0) {
+      this.setData({ showCart: false });
+    }
+  },
+
+  // Submit
   async onSubmitOrder() {
+    await this.submitOrder();
+  },
+
+  async onSubmitFromCart() {
+    await this.submitOrder();
+  },
+
+  async submitOrder() {
     if (this.data.selectedIds.length === 0) {
       wx.showToast({ title: '请先选择菜品', icon: 'none' });
       return;
@@ -76,10 +148,12 @@ Page({
 
     const items = this.data.selectedIds.map(id => {
       const recipe = this.data.recipes.find(r => r._id === id);
+      const info = this.data.selectedRecipes[id] || { quantity: 1, note: '' };
       return {
         recipeId: id,
         name: recipe.name,
-        note: this.data.selectedRecipes[id]?.note || '',
+        quantity: info.quantity,
+        note: info.note,
       };
     });
 
@@ -96,6 +170,15 @@ Page({
     }
 
     wx.showToast({ title: '点菜成功', icon: 'success' });
-    this.setData({ selectedIds: [], selectedRecipes: {}, totalCount: 0, totalMinutes: 0 });
+    this.setData({
+      selectedIds: [],
+      selectedRecipes: {},
+      totalCount: 0,
+      totalMinutes: 0,
+      showCart: false,
+      cartItems: [],
+    });
   },
+
+  noop() {},
 });
